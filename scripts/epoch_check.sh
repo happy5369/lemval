2991f0224f45a4b38397fed659b8d841d47e86c5
happy5369 <higuysthisismyemail@proton.me>
2025-01-28 17:42:23 -0800

    added staked and val numbers to epoch

--- scripts/epoch_check.sh
+++ scripts/epoch_check.sh
@@ -7,20 +7,35 @@ f_checkAndSetBuildTimestamp "$1"
 typeset output_filename=$(f_getRunOutputFilename_Helper "epoch")
 {
 	while [[ 1 ]]; do
-		current_epoch=$(~/go-opera/build/opera attach --preload /extra/preload.js --datadir=/extra/lemon/data --exec "sfcc.currentEpoch();")
+		current_epoch=$(f_runOpera "sfcc.currentEpoch();")
 		current_time=`date +%s`
 		
 		if [[ $previous_epoch != $current_epoch ]]; then
-			current_block=$(~/go-opera/build/opera attach --preload /extra/preload.js --datadir=/extra/lemon/data --exec "ftm.blockNumber;")
+			# time
+			epoch_time_length=$(((current_time-previous_time)/60))
+			previous_time=$current_time
+			
+			# blocks
+			current_block=$(f_runOpera "ftm.blockNumber;")
 			blocks=$((current_block-previous_block))
 			previous_block=$current_block
+			
+			# staked
+			current_staked=$(f_runOpera "sfcc.totalStake();")
+			current_staked=$(f_convertLemNumber "$current_staked")
+			current_staked=$(f_round "$current_staked" "0")	# no decimals 
+			staked=$((current_staked-previous_staked))
+			previous_staked=$current_staked
+			
+			# vals
+			current_vals=$(f_getTotalActiveVals)
+			vals=$((current_vals-previous_vals))
+			previous_vals=$current_vals
 		
-			epoch_time_length=$(((current_time-previous_time)/60))
-		
-			echo "$previous_epoch: $blocks ${epoch_time_length}m"
+			f_printDate
+			printf "|%-4s| %3sm %-4s %-4s $vals\n" "$previous_epoch" "$epoch_time_length" "$blocks" "$staked"
 			
 			previous_epoch=$current_epoch
-			previous_time=$current_time
 		fi
 		
 		sleep 1m;
