5767197cfb1cdde778b64788586a53f5a6ec5d81
happy5369 <higuysthisismyemail@proton.me>
2025-02-01 18:22:26 -0800

    added printing of totals

--- scripts/rewards_check_active_vals.sh
+++ scripts/rewards_check_active_vals.sh
@@ -25,11 +25,19 @@ function f_calculateRewards {
 	f_printRewards "$val_id" "$pending_rewards" "$previous_rewards" "$epoch" "$getStake" "$getUnlockedStake" "$getLockedStake" "$getDelegated" "$decimal_places" "false"
 }
 
+function f_touchupFile {
+	typeset file=$1
+	
+	sed -i 's#id=#xxxx#' $file
+	sed -i 's#=# #g' $file
+}
+
 f_checkAndSetBuildTimestamp "$1"
 
 typeset active_vals_file="/tmp/active_vals.out"
 typeset tmp_file1="/tmp/rewards_check_active_vals.out"
 typeset tmp_file2="${tmp_file1}2"
+typeset tmp_file3="${tmp_file1}3"
 
 typeset output_filename=$(f_getRunOutputFilename_Helper "rewards_check_active_vals")
 {
@@ -50,9 +58,27 @@ typeset output_filename=$(f_getRunOutputFilename_Helper "rewards_check_active_va
 				typeset      val_id=`echo "$line" | awk '{print $1}' | tr -d ":"`
 				typeset val_address=`echo "$line" | awk '{print $8}' | tr -d "\"" | tr -d "]"`
 				typeset rewards=`f_calculateRewards "$val_id" "$val_address" "$previous_epoch" 4`
-				echo "$rewards"	# quotes important or else it won't do printf formatting properly
+				echo "$rewards"	# quotes important or else it won't do printf formatting properly 
 				echo "$rewards" >> $tmp_file2
 			done < "$active_vals_file"
+			
+			cp $tmp_file2 $tmp_file3
+			f_touchupFile "$tmp_file3"
+			typeset       total_sum=`awk '{s+=$5}END{print s}'  $tmp_file3`
+			typeset       epoch_sum=`awk '{s+=$8}END{print s}'  $tmp_file3`
+			typeset    total_staked=`awk '{s+=$12}END{print s}' $tmp_file3`
+			typeset  total_unlocked=`awk '{s+=$14}END{print s}' $tmp_file3`
+			typeset    total_locked=`awk '{s+=$16}END{print s}' $tmp_file3`
+			typeset total_delegated=`awk '{s+=$18}END{print s}' $tmp_file3`
+			typeset    total_shared=`awk '{s+=$20}END{print s}' $tmp_file3`
+			#printf "rewards: total=%s, epoch=%s\n" "$total_sum" "$epoch_sum"
+			typeset rewardsPrintoutFormat=`f_getRewardsPrintoutFormat`
+			echo -n "---------------- "
+			printf "$rewardsPrintoutFormat" "|$previous_epoch|" "" "$total_sum" "" "$epoch_sum" "" "$total_staked" "$total_unlocked" "$total_locked" "$total_delegated" "$total_shared"
+			#printf "rewards: total=%s, epoch=%s\n" "$total_sum" "$epoch_sum" >> $tmp_file3
+			#printf "staked:  total=%s, delegated=%s\n" "$total_staked" "$total_delegated"
+			
+			rm $tmp_file3
 			
 			echo
 			previous_epoch=$current_epoch
