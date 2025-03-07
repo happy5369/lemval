4c1a0ec184db920d888ca1eb37d83619c1e99349
happy5369 <higuysthismyemail@proton.me>
2025-03-06 18:22:45 -0800

    adding projections

--- scripts/daily_rewards_total_all.sh
+++ scripts/daily_rewards_total_all.sh
@@ -16,11 +16,24 @@ typeset output_filename=$(f_getRunOutputFilename_Helper "daily_rewards_total_all
 	
 	typeset current_epoch=0
 	
+	typeset staked_pool=20000
+	typeset locked_pool=200000
+	typeset yearly_expected=$((staked_pool+locked_pool))
+	typeset daily_expected=$(f_compute "$yearly_expected/365")
+	
+	echo "========== Expected =========="
+	echo "$yearly_expected"
+	echo "$daily_expected"
+	
 	while [[ 1 ]]; do
-		typeset total=`f_getRewardsPayoutTotal "$LEM_TMP_AREA/$filename" "$current_epoch"`
+		typeset total_daily=`f_getRewardsPayoutTotal "$LEM_TMP_AREA/$filename" "$current_epoch"`
+		typeset total_yearly=`f_compute "$total_daily*365"`
+		
+		typeset diff_daily=$(f_compute "$total_daily-$daily_expected")
+		typeset diff_yearly=$(f_compute "$total_yearly-$yearly_expected")
 		
 		f_printDate
-		printf "|%-4s| %4s\n" "$current_epoch" "$total"
+		printf "|%-4s| %4.3f %6.3f (%2.2f %s)\n" "$current_epoch" "$total_daily" "$total_yearly" "$diff_daily" "$diff_yearly"
 		
 		current_epoch=`grep -F '-' $LEM_TMP_AREA/$filename | tail -n 1 | cut -d ' ' -f3 | tr -d '|'`
 		
