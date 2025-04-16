c44020cadb41c559e06d4e123e06d2d360aacf8f
happy5369 <higuysthismyemail@proton.me>
2025-04-16 12:04:29 -0700

    updated to be generic for >1TB disks

--- scripts/disk_check.sh
+++ scripts/disk_check.sh
@@ -9,7 +9,7 @@ f_checkAndSetBuildTimestamp "$1"
 typeset output_filename=$(f_getRunOutputFilename_Helper "disk")
 {
 	while [[ 1 ]]; do
-		current=`df -H | grep sda2`
+		current=`df -H | grep $LEM_STORAGE_DRIVE`
 		if [[ $diffs_only == false || ($diffs_only == true && "$current" != "$previous") ]]; then		
 			f_printDate
 			echo $current
