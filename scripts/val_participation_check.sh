33ca5b6a5c78a76aafd74fbd7058dd31540b5628
happy5369 <higuysthismyemail@proton.me>
2025-05-18 18:57:23 -0700

    added param check, and also refactored variable name

--- scripts/val_participation_check.sh
+++ scripts/val_participation_check.sh
@@ -4,16 +4,23 @@ source lib/common.lib
 
 f_checkAndSetBuildTimestamp ""
 
-typeset output_filename=$(f_getRunOutputFilename_Helper "consensus_check")
+typeset output_filename=$(f_getRunOutputFilename_Helper "val_participation_check")
 {
+	# cmdline param
 	typeset num_epochs_to_check=$1
+
+	if [[ -z $num_epochs_to_check ]]; then
+		echo "need a number!"
+		echo "num_epochs_to_check=$num_epochs_to_check"
+		exit
+	fi
 
 	typeset sealed_epoch=$(f_opera_getSealedEpoch)
 	typeset stop_epoch=$((sealed_epoch-num_epochs_to_check))
 	
-	for ((i=$sealed_epoch; i>$stop_epoch; i--)); do 
-		typeset validator_id_list=$(f_opera_getEpochValidatorIds "$i")
-		echo "|$i| $validator_id_list"
+	for ((epoch=$sealed_epoch; epoch>$stop_epoch; epoch--)); do 
+		typeset validator_id_list=$(f_opera_getEpochValidatorIds "$epoch")
+		echo "|$epoch| $validator_id_list"
 	done
 
 } 2>&1 | tee $output_filename
