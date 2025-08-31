16c051adc1881d9f9bc71f00967f43629bc7914f
happy5369 <higuysthisismyemail@proton.me>
2025-08-30 19:21:46 -0700

    specifically calling out awk -> gawk

--- scripts/sensors_check_hot.sh
+++ scripts/sensors_check_hot.sh
@@ -18,7 +18,7 @@ typeset output_filename=$(f_getRunOutputFilename_Helper "sensors_hot")
 	filename=$(f_resolveFilename "$filename")
 	
 	# stdbuf -oL a is necessary or else won't dump to file continuously 
-	tail -f $filename | stdbuf -oL awk '
+	tail -f $filename | stdbuf -oL gawk '
 	  function red(text) { return "\033[31m" text "\033[0m" }
 	  {
 	    match($0, /cpus \(([0-9.]+) *\/ *([0-9.]+)\)/, c)
@@ -51,4 +51,4 @@ typeset output_filename=$(f_getRunOutputFilename_Helper "sensors_hot")
 	  }
 	'
 } 2>&1 | tee $output_filename
-# } 2>&1 | stdbuf -oL tee $output_filename - could do stdbuf -oL on tee as well, but it seems to not need it, only awk needs it
+# } 2>&1 | stdbuf -oL tee $output_filename - could do stdbuf -oL on tee as well, but it seems to not need it, only gawk needs it
